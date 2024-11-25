4d67d58408e33875343e859f72466b65fcbbd89e
han253 <han253@gmail.com>
2024-11-25 11:35:55 -0500

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotasComponent } from './notas/notas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotasComponent
   ],
   imports: [
     BrowserModule,
